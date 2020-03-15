727725836b024bc54b8b26667bbfabd07671b98b
tom02850tom02850 <lespaul.ctd.mh-5010t.overdrive@docomo.ne.jp>
2020-03-15 20:31:48 +0900

    自動通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,4 +64,29 @@ $('#newmessage').on('submit', function(e){
     alert('error');
   });
 })
+var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.main-chat_messages').append(insertHTML);  
+    $('.main-chat_messages').animate({ scrollTop: $('.main-chat_messages')[0].scrollHeight});
+  }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
